b379712e9cbbd97ada88b3f4347b0704f8127860
Zehan Khan <zehan9211@gmail.com>
2023-01-17 18:54:16 +0530

    add 5 letter words json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="mx-auto w-96 relative h-screen">
       <header className="border-b border-gray-400 py-4">
