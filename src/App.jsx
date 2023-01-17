9343f1dcef8bfd3fb5f2cd1b54822b1e41f7b897
Zehan Khan <zehan9211@gmail.com>
2023-01-17 16:26:11 +0530

    app intialized with vite and tailwind css setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App text-red-700">
+      <div>
+        Wordley
+      </div>
+    </div>
+  )
+}
+
+export default App
