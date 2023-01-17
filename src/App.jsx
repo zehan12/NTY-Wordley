6fe887644feb3f5d26d48943c37d2cb93bc01b77
Zehan Khan <zehan9211@gmail.com>
2023-01-17 18:14:59 +0530

    setup vitest with jest, testing library

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App text-red-700">
-      <div>
-        Wordley
-      </div>
+    <div className="mx-auto w-96 relative h-screen">
+      <header className="border-b border-gray-400 py-4">
+        <h1 className="text-3xl font-bold text-center uppercase">Wordle</h1>
+      </header>
     </div>
   )
 }
